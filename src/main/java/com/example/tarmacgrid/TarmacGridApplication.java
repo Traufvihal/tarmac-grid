97f3c8f388cb56ff327a7a08e5a421acb4aecc12
Traufvihal <santiago.sansone@gmail.com>
2018-09-17 20:18:26 -0300

    Initial setup of the project

---
+++ src/main/java/com/example/tarmacgrid/TarmacGridApplication.java
@@ -0,0 +1,12 @@
+package com.example.tarmacgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarmacGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarmacGridApplication.class, args);
+	}
+}
